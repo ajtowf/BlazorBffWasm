f3e927075e138ca64f286e0a99855a4b214ccc58
Ajden Towfeek <ajden@towfeek.se>
2026-02-19 01:50:51 +0100

    Cleanup.

--- IntegrationTests.e2e/Setup/SharedCollectionFixture.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IntegrationTests.e2e.Setup;
-
-[CollectionDefinition(Name, DisableParallelization = true)]
-public class SharedCollectionFixture : ICollectionFixture<SharedFixture>
-{
-    public const string Name = "Shared collection";
-}
